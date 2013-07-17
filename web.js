83b13b569d48d88f0b0fdcb99707931b27d6f476
rodthomas <rod@rod-virtual-machine.(none)>
2013-07-17 16:33:49 -0700

    modified web.js

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var fs = require('fs');
+
+app.get('/', function(req, res) {
+    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
