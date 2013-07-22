310960c245c3822d8f472e42825087b82590a427
rodthomas <rod@rod-virtual-machine.(none)>
2013-07-22 14:32:07 -0700

    Updated Head tag with Bootstrap info

--- web.js
+++ web.js
@@ -12,8 +12,7 @@ app.get('/', function(req, res) {
 });
 
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
 
+var port = process.env.PORT || 5000;
+app.listen(port, function() console.log("Listening on " + port);
+});
