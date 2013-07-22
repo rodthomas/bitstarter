0a648af38068436f65d59f8c81fb7248f71e8101
rodthomas <rod@rod-virtual-machine.(none)>
2013-07-21 20:49:45 -0700

    modified web.js

--- web.js
+++ web.js
@@ -12,7 +12,8 @@ app.get('/', function(req, res) {
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
